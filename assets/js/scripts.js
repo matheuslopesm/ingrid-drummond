011ece1bd8b6e616e93f114c3430e8da3fa62954
Matheus Lopes <matheus.lopesmdev@gmail.com>
2023-01-04 21:31:20 -0300

    Edição no Formulário de Agendamento

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,6 +6,20 @@ function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
 
+function abrirWhatsapp() {
+  var nome = document.getElementById("form-Name").value;
+  var email = document.getElementById("form-email").value;
+  var telefone = document.getElementById("form-tel").value;
+  var dataDesejada = document.getElementById("form-date").value;
+  // var url = "https://wa.me/5531989773977?text=" // Seu número
+  var url = "https://wa.me/5533991384321?text=" // Seu número
+    + "Olá, me chamo " + nome + " e gostaria de marcar uma consulta para o dia " + dataDesejada + "! %0a"
+    + "%0a" // Quebra de linha
+    + "*E-mail*: " + email + "%0a"
+    + "*Telefone*: " + telefone + "%0a"
+  window.open(url, '_blank').focus();
+}
+
 ScrollReveal({
   origin: 'top',
   distance: '50px',
